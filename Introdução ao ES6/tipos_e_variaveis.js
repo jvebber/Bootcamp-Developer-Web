eef80aae355bcfe629efeb43466fc81c8729ea84
jvebber <jvebber@hotmail.com>
2022-02-16 16:00:10 -0300

    commit tipos e variaveis

---
+++ Introdução ao ES6/tipos_e_variaveis.js
@@ -0,0 +1,23 @@
+// ------ STRINGS ------
+
+const splittedText = 'Texto'.split('x');
+console.log('\n Array com as posições separadas pelo delimitador:', splittedText);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
