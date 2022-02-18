94fa701198d4f307d0993a0659081e38eb3e959e
jvebber <jvebber@hotmail.com>
2022-02-18 17:52:14 -0300

    commit inicial do arquivo estruturas_de_repeticao

---
+++ Introdução ao ES6/estruturas_de_repeticao.js
@@ -0,0 +1,53 @@
+//*********ESTRUTURAS de REPETIÇÃO********** //
+
+/*
+- FOR
+- WHILE
+- DO...WHILE
+- FOR...IN/OF
+- CONTINUE
+- BREAK
+*/
+
+//For
+
+const numbers = [2,3,4,5,6,8,9,10,15,25];
+
+for (let index = 0; index < numbers.length; index++) {
+    const element = numbers[index];
+    console.log(`O elemento do array é ${element}`);
+}
+
+
+//While
+var index = 0;
+while (index < numbers.length) {
+    console.log("Lendo array com while, elemento:", numbers[index]);
+    index++;
+}
+
+
+
+//Do...While
+var index = 0;
+
+do {
+    console.log("Lendo array com Do...while, elemento:", numbers[index]);
+    index++;
+} while (index < numbers.length);
+
+numbers.foo = "Hello"; // para aplicar for in and for of logo abaixo
+
+//for in 
+for (var key in numbers) {
+        console.log("Lendo array com For In, elemento:", key);
+}
+
+
+//for of
+for (var key of numbers) {
+        console.log("Lendo array com For OF, elemento:", key);
+}
+
+
+
