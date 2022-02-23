31f1c07116011d2d5cd74f242c59995833173409
jvebber <jvebber@hotmail.com>
2022-02-23 15:04:43 -0300

    Commit inicial design patters

---
+++ Introdução ao ES6/design_patters.js
@@ -0,0 +1,26 @@
+/*
+
+Design Patters (padrões de projetos): Não de trata de um framework ou um código pronto, mas é uma definição
+em mais alto nível de como um problema comum pode ser solucionado.
+
+TIPOS:
+
+- Criação: abstraem e/ou adiam o processo de criação de objetos
+- Estruturais: se preocupam com a forma como classes e objetos são compostos para formar estruturas maiores
+- Comportamentais: se concentram nos algoritmos e atribuições de responsabilidade entre os objetos. Descrevem os padrões
+de comunicação entre os objetos
+
+
+Mais usados:
+- Factory      Criação 
+- Singleton    Criação 
+- Decorator    Estrutural
+- Observer     Comportamental 
+- Module
+
+*/
+
+
+
+
+
