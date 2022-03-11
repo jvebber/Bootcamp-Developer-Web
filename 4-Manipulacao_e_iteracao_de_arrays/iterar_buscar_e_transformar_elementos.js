7b5ab23928bece83becf8490b928727093c3e9a5
jvebber <jvebber@hotmail.com>
2022-03-11 17:00:44 -0300

    feat: corrigi nome do arquivo

---
+++ 4-Manipulacao_e_iteracao_de_arrays/iterar_buscar_e_transformar_elementos.js
@@ -0,0 +1,27 @@
+//ITERAR ELEMENTOS
+
+
+//forEach - itera cada item de um array sem alterá-lo
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+numbers.forEach((number, index, numbers) => {
+    console.log(`${index}: ${number}: array: ${numbers}`);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
