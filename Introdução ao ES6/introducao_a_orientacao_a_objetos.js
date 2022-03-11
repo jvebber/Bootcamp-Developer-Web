c9d5d77551d04c65a48b15852596898ba6faf4f2
jvebber <jvebber@hotmail.com>
2022-03-11 09:34:41 -0300

    feat: atualizando com revisão da aula

--- Introdução ao ES6/introducao_a_orientacao_a_objetos.js
+++ Introdução ao ES6/introducao_a_orientacao_a_objetos.js
@@ -8,7 +8,8 @@
 
 */
 
-class Person {
+//CLASSES
+class Person {      //Exemplo 1 - Classes
     constructor(name){
         this.name = name;
     }
@@ -25,4 +26,56 @@ const jocenei = new PersonF("Jocenei", "055.505.329-18");
 
 console.log(jocenei);
 
+
+class Animal{       //Exemplo 2 - Classes
+    constructor(){
+        this.qtdePatas = 4;
+    }
+}
+
+class Dog extends Animal {
+    constructor(name,breed){
+        super();
+        this.name = name;
+        this.breed = breed;
+    }
+
+    latir(){
+        return "Au, Au, Au";
+    }
+    
+}
+
+const pitbull = new Dog("Negão", "Pitbull");
+
+//console.log (pitbull.latir());
+
+console.log(`Nome: ${pitbull.name}, Raça: ${pitbull.breed}, Som: ${pitbull.latir()}`, );
+
+
+//MODIFICADORES DE ACESSO E ESCAPSULAMENTO
+
+class Person{
+    #name='';
+
+    constructor(name){
+        this.#name = name;
+    }
+
+    set name(name){
+        this.#name = name;
+    }
+
+    get name(){
+        return this.#name;
+    }
+}
+
+const person1 = new Person("Jocenei");
+
+console.log (person1.name);//Jocenei
+
+person1.name = "Guilherme";
+
+console.log (person1.name);//Guilherme
 
